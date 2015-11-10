483ae576d1f05531382696e8bdbb3799e6b7c11b
Doug Grainger <dhgrainger@gmail.com>
2015-11-09 22:47:07 -0500

    refactor hangman

--- hangman.rb
+++ hangman.rb
@@ -1,174 +1,163 @@
 #!/usr/bin/env ruby
+require 'pry'
 
-words = %w(
- random
- india
- america
- cricket
- football
- friday
- sunday
- sausage
- blubber
- pencil
- cloud
- moon
- water
- computer
- school
- network
- hammer
- walking
- violently
- mediocre
- literature
- chair
- two
- window
- cords
- musical
- zebra
- xylophone
- penguin
- home
- dog
- final
- ink
- teacher
- fun
- website
- banana
- uncle
- softly
- mega
- ten
- awesome
- attatch
- blue
- internet
- bottle
- tight
- zone
- tomato
- prison
- hydro
- cleaning
- telivision
- send
- frog
- cup
- book
- zooming
- falling
- evily
- gamer
- lid
- juice
- moniter
- captain
- bonding
- loudly
- thudding
- guitar
- shaving
- hair
- soccer
- water
- racket
- table
- late
- media
- desktop
- flipper
- club
- flying
- smooth
- monster
- purple
- guardian
- bold
- hyperlink
- presentation
- world
- national
- comment
- element
- magic
- lion
- sand
- crust
- toast
- jam
- hunter
- forest
- foraging
- silently
- joshing
- pong
- )
+class Hangman
 
-string = words.sample
-array = string.split(//)
-array2 = []
-missed_letters =[]
-chances = array.length
-x = 0
+	def initialize
+		@library =
+		 %w(
+		 random
+		 india
+		 america
+		 cricket
+		 football
+		 friday
+		 sunday
+		 sausage
+		 blubber
+		 pencil
+		 cloud
+		 moon
+		 water
+		 computer
+		 school
+		 network
+		 hammer
+		 walking
+		 violently
+		 mediocre
+		 literature
+		 chair
+		 two
+		 window
+		 cords
+		 musical
+		 zebra
+		 xylophone
+		 penguin
+		 home
+		 dog
+		 final
+		 ink
+		 teacher
+		 fun
+		 website
+		 banana
+		 uncle
+		 softly
+		 mega
+		 ten
+		 awesome
+		 attatch
+		 blue
+		 internet
+		 bottle
+		 tight
+		 zone
+		 tomato
+		 prison
+		 hydro
+		 cleaning
+		 telivision
+		 send
+		 frog
+		 cup
+		 book
+		 zooming
+		 falling
+		 evily
+		 gamer
+		 lid
+		 juice
+		 moniter
+		 captain
+		 bonding
+		 loudly
+		 thudding
+		 guitar
+		 shaving
+		 hair
+		 soccer
+		 water
+		 racket
+		 table
+		 late
+		 media
+		 desktop
+		 flipper
+		 club
+		 flying
+		 smooth
+		 monster
+		 purple
+		 guardian
+		 bold
+		 hyperlink
+		 presentation
+		 world
+		 national
+		 comment
+		 element
+		 magic
+		 lion
+		 sand
+		 crust
+		 toast
+		 jam
+		 hunter
+		 forest
+		 foraging
+		 silently
+		 joshing
+		 pong
+		 )
 
-	while x < array.length
-		array2 << "-"
-		x +=1
+		@word = @library.sample.split('')
+		@guesses =[]
 	end
 
-	def printGuess(array2)
-		array2.each do |x|
-			print x
-		end
+	def chances
+		@word.length - @guesses.length + (@guesses & @word).length
 	end
-puts "===========WELCOME TO HANGMAN==========="
 
-printGuess(array2)
-
-until chances == 0 || array2.include?("-")==false
-
-	puts "\n Guess a letter(a-z). Or if your feeling lucky you can guess the word.\n Chances left: #{chances} Missed Letters: #{missed_letters}.\n "
-	letter = gets.chomp.to_s
-
-	if letter.length == 1
-		if array.include?(letter)
-			y = 0
-			array.each do |x|
-				if x == letter
-					array2[y] = letter
-				end
-				y+=1
+	def display_word
+		@word.each do |letter|
+			if @guesses.include?(letter)
+				print letter
+			else
+			  print '-'
 			end
-			printGuess(array2)
-		else
-			chances -= 1
-			missed_letters << letter
-			printGuess(array2)
 		end
+		puts "\n Guess a letter(a-z). Or if your feeling lucky you can guess the word.\nChances left: #{chances}\nMissed Letters: #{@guesses - @word}."
+	end
 
-		if array2.include?("-")==false
-			puts "\n    YOU WIN!!!!!"
+	def prompt_guess
+		display_word
+		@guesses << gets.chomp
+		if !(@word & @guesses).empty?
+			winner?
+		else
+			loser?
 		end
 	end
 
-	if letter.length > 1
-		if letter == string
-			puts string
-			puts "      YOU WIN!!!!!"
-			break
+	def winner?
+		if (@word & @guesses) == @word.uniq
+			puts " #{@word.join('')} YOU WIN!!!!!"
 		else
-			chances = 0
+			prompt_guess
 		end
 	end
 
-
+	def loser?
+		if chances > 0
+			prompt_guess
+		else
+			puts " #{@word.join('')} you lose WOMMMP WOMMMPPP WOMMMMPPPP WOMMMMMPPPPMPMPMPMP (Sad Trombone)"
+		end
+	end
 end
 
-if chances == 0
-	puts "\n sorry you lose the word was #{string}"
-end
+hangman = Hangman.new
 
-puts "==========THANKS FOR PLAYING=========="
+hangman.prompt_guess
